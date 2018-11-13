68f858b5772ab1d36bc90a57275ca341a18805f5
dclonyx <dcl.fabieno@18pixel.fr>
2018-11-13 12:17:26 +0100

    mise en place css

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" href="./css/style.css">
 	<title>Files Explorer</title>
 </head>
 <body>
@@ -9,17 +10,32 @@
 		<p id="path"></p>
 	</header>
 	<main>
-		<?php
+		<!-- <?php
 			include 'php/dir_fct.php';
-		?>
+		?> -->
+		<div class="dossier"><img src="./img/icone dossier.png" alt="">
+			<p>titre du dossier</p>
+			<p>infos</p>
+		</div>
+
+		<div class="dossier"><img src="./img/icone dossier.png" alt="">
+			<p>titre du dossier</p>
+			<p>infos</p>
+		</div>
+
+		<div class="dossier"><img src="./img/icone dossier.png" alt="">
+			<p>titre du dossier</p>
+			<p>infos</p>
+		</div>
 	</main>
 	<footer>
 		<div id="menu">
 			<p>Menu</p>
 		</div>
 		<div id="back">
-			<p>Retour</p>
+			<input type="image" value="Retour" src="img/arrow.svg" alt="">
 		</div>
 	</footer>
+	<script src="./js/script.js"></script>
 </body>
 </html>
